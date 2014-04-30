407f9dcfc4a4d9a8f55835966420d7651f5f2090
sikessle <sikessle@172.20.153.240>
2014-04-30 09:51:33 +0200

    persistence configuration set to CouchDB

--- src/org/sikessle/gameoflife/GameOfLife.java
+++ src/org/sikessle/gameoflife/GameOfLife.java
@@ -2,6 +2,8 @@ package org.sikessle.gameoflife;
 
 import org.sikessle.gameoflife.controller.GridController;
 import org.sikessle.gameoflife.persistence.couchdb.CouchDBModule;
+import org.sikessle.gameoflife.persistence.dummy.DummyModule;
+import org.sikessle.gameoflife.view.gui.SwingView;
 import org.sikessle.gameoflife.view.tui.TextView;
 
 import com.google.inject.Guice;
@@ -11,10 +13,10 @@ public class GameOfLife {
 
 	public static void main(String[] args) {
 
-		Injector injector = Guice.createInjector(new BaseModule(),
-				new CouchDBModule());
+		Injector injector = Guice.createInjector(new CouchDBModule(),
+				new DummyModule());
 		GridController controller = injector.getInstance(GridController.class);
-		// new SwingView(controller);
+		new SwingView(controller);
 		TextView textUI = new TextView(controller);
 		textUI.readAndInterpretInLoopFromInputStream();
 	}
