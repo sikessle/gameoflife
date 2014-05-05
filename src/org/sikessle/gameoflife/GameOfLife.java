746df164d3c373a58b7f214eb77283bfc42a2a66
sikessle <sikessle@MBA-silver.local>
2014-05-05 10:15:11 +0200

    performance profiling of databases

--- src/org/sikessle/gameoflife/GameOfLife.java
+++ src/org/sikessle/gameoflife/GameOfLife.java
@@ -2,10 +2,14 @@ package org.sikessle.gameoflife;
 
 import org.apache.log4j.PropertyConfigurator;
 import org.sikessle.gameoflife.controller.GridController;
+import org.sikessle.gameoflife.persistence.couchdb.CouchDBModule;
 import org.sikessle.gameoflife.persistence.db4o.Db4oModule;
+import org.sikessle.gameoflife.persistence.dummy.DummyModule;
+import org.sikessle.gameoflife.persistence.hibernate.HibernateModule;
 import org.sikessle.gameoflife.view.gui.SwingView;
 import org.sikessle.gameoflife.view.tui.TextView;
 
+import com.db4o.Db4o;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
@@ -18,7 +22,7 @@ public final class GameOfLife {
 		PropertyConfigurator.configure("log4j.properties");
 
 		Injector injector = Guice.createInjector(new BaseModule(),
-				new Db4oModule());
+				new CouchDBModule());
 		GridController controller = injector.getInstance(GridController.class);
 		new SwingView(controller);
 		TextView textUI = new TextView(controller);
